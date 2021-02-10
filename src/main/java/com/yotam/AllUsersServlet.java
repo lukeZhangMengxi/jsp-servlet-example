2aacd8afa73a30581b9a71da259f21ed28231767
Mengxi Zhang <zmx94824@gmail.com>
2021-02-09 22:06:41 -0800

    Cookie tutorial: set/get cookie in req/rsp

--- src/main/java/com/yotam/AllUsersServlet.java
+++ src/main/java/com/yotam/AllUsersServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
+
 
 /**
  * Created by yotamm on 28/04/16.
